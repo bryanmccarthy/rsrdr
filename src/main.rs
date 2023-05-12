a72538141158ffd06cf4c3d432a8842970fe6852
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-05-12 18:06:59 +0100

    set title

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use tui::{
     backend::CrosstermBackend,
     widgets::{Widget, Block, Borders},
     layout::{Layout, Constraint, Direction},
+    text::Span,
+    style::Style,
     Terminal
 };
 use crossterm::{
@@ -12,6 +14,7 @@ use crossterm::{
 };
 
 fn main() -> Result<(), io::Error> {
+    // Set block
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
@@ -21,13 +24,15 @@ fn main() -> Result<(), io::Error> {
     terminal.draw(|f| {
         let size = f.size();
         let block = Block::default()
-            .title("Block")
+            .title(Span::styled("rsrdr", Style::default()))
             .borders(Borders::ALL);
         f.render_widget(block, size);
     })?;
 
+    // sleep
     thread::sleep(Duration::from_millis(5000));
 
+    // Restore terminal 
     disable_raw_mode()?;
     execute!(
         terminal.backend_mut(),
